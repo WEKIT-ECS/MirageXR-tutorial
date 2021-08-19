4205555a391a8632b4fe54916643962acb5d871d
aradjafari <arad.jafari@gmail.com>
2021-08-19 21:05:16 +0200

    Some of editors are added. Screenshot capture is added to the screen

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class SceneManager : MonoBehaviour
@@ -7,6 +8,11 @@ public class SceneManager : MonoBehaviour
 
     [SerializeField] private GameObject uploadingPanel;
     [SerializeField] private GameObject termOfUSePabel;
+
+    [SerializeField] private GameObject audioEditorPanel;
+    [SerializeField] private GameObject audioEditorSettingPanel;
+
+    [SerializeField] private GameObject onScreenCanvas;
 
     public void ToggleUploadingPanelVisibility()
     {
@@ -16,5 +22,37 @@ public class SceneManager : MonoBehaviour
     public void ToggleTermOfUsePanelVisibility()
     {
         termOfUSePabel.SetActive(!termOfUSePabel.activeInHierarchy);
+    }
+
+    public void ToggleAudioEditorSettingPanelVisibility()
+    {
+        audioEditorPanel.SetActive(!audioEditorPanel.activeInHierarchy);
+        audioEditorSettingPanel.SetActive(!audioEditorSettingPanel.activeInHierarchy);
+    }
+
+    public void CaptureScreenShot()
+    {
+        StartCoroutine(Capture());
+    }
+
+    IEnumerator Capture()
+    {
+        onScreenCanvas.SetActive(false);
+        var random = new System.Random();
+
+        string folderPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop) + "/Screenshots/";
+
+        if (!System.IO.Directory.Exists(folderPath))
+            System.IO.Directory.CreateDirectory(folderPath);
+
+        var screenshotName =
+                                "Screenshot_" +
+                                System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss") +
+                                ".png";
+        ScreenCapture.CaptureScreenshot(System.IO.Path.Combine(folderPath, screenshotName),2);
+        Debug.Log(folderPath + screenshotName);
+
+        yield return new WaitForSeconds(0.2f);
+        onScreenCanvas.SetActive(true);
     }
 }
